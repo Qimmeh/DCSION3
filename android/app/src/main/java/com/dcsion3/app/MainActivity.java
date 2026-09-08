110435cc5124176f455d5ca3ca235cad7381c392
Hing Zi Feng <zfhing2107@gmail.com>
2026-09-08 17:17:35 +0800

    feat(mobile): build Android homescreen gadget (AppWidget) and ambient companion controller

--- android/app/src/main/java/com/dcsion3/app/MainActivity.java
+++ android/app/src/main/java/com/dcsion3/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.dcsion3.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(AmbientWidgetPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
